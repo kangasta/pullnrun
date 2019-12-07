f8bc210d47d4419c90f3167968a2cd1dbba83cf6
Toni Kangas <toni.ar.kangas@gmail.com>
2019-12-07 17:51:09 +0200

    Modify push to support S3 signed urls

--- pullnrun/_push.py
+++ pullnrun/_push.py
@@ -3,16 +3,17 @@ from shutil import make_archive
 
 from ._utils import timestamp, create_meta
 
-def _push_http(filename, url, method='PUT', headers=None):
+def _push_http(filename, url, method='PUT', data=None, headers=None):
     ok = True
     status = None
 
     start = timestamp()
     try:
         with open(filename, 'rb') as f:
-            r = request(method, url, data=f, headers=headers)
-            r.raise_for_status()
+            files = {'file': (filename, f)}
+            r = request(method, url, data=data, headers=headers, files=files)
             status = r.status_code
+            r.raise_for_status()
     except:
         ok = False
     end = timestamp()
@@ -22,6 +23,7 @@ def _push_http(filename, url, method='PUT', headers=None):
         'ok': ok,
         'data': {
             'url': url,
+            'file': filename,
             'status': status,
         },
         'meta': create_meta(start, end)
@@ -30,7 +32,7 @@ def _push_http(filename, url, method='PUT', headers=None):
 def push(**kwargs):
     to = kwargs.get('to')
     if to == 'url':
-        keys = ('filename', 'url', 'method', 'headers', )
+        keys = ('filename', 'url', 'method', 'data', 'headers', )
         return _push_http(**{k: v for k, v in kwargs.items() if k in keys})
     elif to == 's3':
         raise NotImplementedError('TODO')
